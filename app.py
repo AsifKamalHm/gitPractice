8f12904431cf3180738f6d862b40026fc57eb5ef
Asif <asif98574@gmail.com>
2026-07-10 13:09:04 +0500

    artifacts workflow exlored

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import os
 
 app = Flask(__name__)
 
+print("Cloud With VarJosh Flask Application Started")
+
 @app.get("/")
 def home():
+    print("Home endpoint invoked")
+
     return jsonify(
         message="Welcome to Cloud With VarJosh",
         platform="GitHub Actions",
@@ -13,6 +17,7 @@ def home():
 
 @app.get("/health")
 def health():
+    print("Health endpoint invoked")
     return jsonify(status="healthy"), 200
 
 if __name__ == "__main__":
